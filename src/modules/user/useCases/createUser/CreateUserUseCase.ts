416572516a510f80f7c8dcaa0cc16b0ec63a68f5
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-17 14:35:23 -0400

    feat: it should throw a validation error when username already exists

--- src/modules/user/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/user/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import prisma from '../../../../client';
 import ValidationError from '../../../../errors/ValidationError';
 import ICreateUserDTO from '../../dtos/ICreateUserDTO';
 
@@ -5,5 +6,13 @@ export default class CreateUserUseCase {
   async execute({ username, password }: ICreateUserDTO): Promise<void> {
     if (username.length < 3)
       throw new ValidationError('Username must be 3 characters or more');
+
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (user != null) throw new ValidationError('Username already exists');
   }
 }
