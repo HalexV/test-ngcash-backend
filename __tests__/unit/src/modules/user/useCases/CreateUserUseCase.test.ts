0e9f6747da80f9a6205adbe58196cc11a2873e28
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-17 16:07:45 -0400

    refactor(test): mock find unique before each test

--- __tests__/unit/src/modules/user/useCases/CreateUserUseCase.test.ts
+++ __tests__/unit/src/modules/user/useCases/CreateUserUseCase.test.ts
@@ -1,10 +1,14 @@
-import { describe, it, expect, jest } from '@jest/globals';
+import { describe, it, expect, jest, beforeEach } from '@jest/globals';
 import prisma from '../../../../../../src/client';
 import ValidationError from '../../../../../../src/errors/ValidationError';
 import CreateUserUseCase from '../../../../../../src/modules/user/useCases/createUser/CreateUserUseCase';
 
 describe('User - Create User Use Case', () => {
-  it('should not create a user when username is less than 3 characters', async () => {
+  beforeEach(() => {
+    jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(null);
+  });
+
+  it('should throw a validation error when username is less than 3 characters', async () => {
     const sut = new CreateUserUseCase();
 
     const userDTO = {
@@ -12,14 +16,18 @@ describe('User - Create User Use Case', () => {
       password: 'any',
     };
 
+    let resultError = new Error();
+
     try {
       await sut.execute(userDTO);
     } catch (error: any) {
-      expect(error).toBeInstanceOf(ValidationError);
-      expect(error.message).toStrictEqual(
-        'Username must be 3 characters or more'
-      );
+      resultError = error;
     }
+
+    expect(resultError).toBeInstanceOf(ValidationError);
+    expect(resultError.message).toStrictEqual(
+      'Username must be 3 characters or more'
+    );
   });
 
   it('should throw a validation error when username already exists', async () => {
