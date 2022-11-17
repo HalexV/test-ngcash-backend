ed7a95938d12319f77e34a3690e0dc213537a0cd
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-17 14:34:41 -0400

    test: it should throw a validation error when username already exists

--- __tests__/unit/src/modules/user/useCases/CreateUserUseCase.test.ts
+++ __tests__/unit/src/modules/user/useCases/CreateUserUseCase.test.ts
@@ -1,4 +1,5 @@
-import { describe, it, expect } from '@jest/globals';
+import { describe, it, expect, jest } from '@jest/globals';
+import prisma from '../../../../../../src/client';
 import ValidationError from '../../../../../../src/errors/ValidationError';
 import CreateUserUseCase from '../../../../../../src/modules/user/useCases/createUser/CreateUserUseCase';
 
@@ -19,5 +20,34 @@ describe('User - Create User Use Case', () => {
         'Username must be 3 characters or more'
       );
     }
+  });
+
+  it('should throw a validation error when username already exists', async () => {
+    const sut = new CreateUserUseCase();
+
+    const userDTO = {
+      username: 'any',
+      password: 'any',
+    };
+
+    const mockUser = {
+      id: 'any',
+      username: 'any',
+      password: 'any',
+      accountId: 'any',
+    };
+
+    jest.spyOn(prisma.user, 'findUnique').mockResolvedValue(mockUser);
+
+    let resultError = new Error();
+
+    try {
+      await sut.execute(userDTO);
+    } catch (error: any) {
+      resultError = error;
+    }
+
+    expect(resultError).toBeInstanceOf(ValidationError);
+    expect(resultError.message).toStrictEqual('Username already exists');
   });
 });
