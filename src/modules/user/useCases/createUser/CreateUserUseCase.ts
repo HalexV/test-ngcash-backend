9eaee55f295a86f65cbc591c52af5f4bd2dd5c16
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-18 09:01:33 -0400

    feat: it should throw a validation error when password is less than 8 characters

--- src/modules/user/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/user/useCases/createUser/CreateUserUseCase.ts
@@ -7,6 +7,11 @@ export default class CreateUserUseCase {
     if (username.length < 3)
       throw new ValidationError('Username must be 3 characters or more');
 
+    if (password.length < 8)
+      throw new ValidationError(
+        'Password must be 8 characters or more and contain a number and an uppercase letter'
+      );
+
     const user = await prisma.user.findUnique({
       where: {
         username,
