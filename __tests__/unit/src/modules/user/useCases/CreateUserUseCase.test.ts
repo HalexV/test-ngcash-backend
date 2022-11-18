234a75d2c2dd62653b7412ef0589178341494111
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-18 13:37:35 -0400

    test: it should throw a validation error when password does not contain an uppercase letter

--- __tests__/unit/src/modules/user/useCases/CreateUserUseCase.test.ts
+++ __tests__/unit/src/modules/user/useCases/CreateUserUseCase.test.ts
@@ -102,4 +102,26 @@ describe('User - Create User Use Case', () => {
       'Password must be 8 characters or more and contain a number and an uppercase letter'
     );
   });
+
+  it('should throw a validation error when password does not contain an uppercase letter', async () => {
+    const sut = new CreateUserUseCase();
+
+    const userDTO = {
+      username: 'any',
+      password: 'invalidabc7',
+    };
+
+    let resultError;
+
+    try {
+      await sut.execute(userDTO);
+    } catch (error: any) {
+      resultError = error;
+    }
+
+    expect(resultError).toBeInstanceOf(ValidationError);
+    expect(resultError.message).toStrictEqual(
+      'Password must be 8 characters or more and contain a number and an uppercase letter'
+    );
+  });
 });
