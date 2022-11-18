e1ba9af681856b04935fd9189fc8c2932ca67c9f
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-18 16:42:51 -0400

    refactor: move validations to validate function

--- src/modules/user/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/user/useCases/createUser/CreateUserUseCase.ts
@@ -3,30 +3,33 @@ import ValidationError from '../../../../errors/ValidationError';
 import ICreateUserDTO from '../../dtos/ICreateUserDTO';
 import bcrypt from 'bcrypt';
 
+async function validate({ username, password }: ICreateUserDTO): Promise<void> {
+  const numberRegexValidator = /[0-9]/;
+  const uppercaseRegexValidator = /[A-Z]/;
+
+  if (username.length < 3)
+    throw new ValidationError('Username must be 3 characters or more');
+
+  if (
+    password.length < 8 ||
+    !numberRegexValidator.test(password) ||
+    !uppercaseRegexValidator.test(password)
+  )
+    throw new ValidationError(
+      'Password must be 8 characters or more and contain a number and an uppercase letter'
+    );
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (user != null) throw new ValidationError('Username already exists');
+}
 export default class CreateUserUseCase {
   async execute({ username, password }: ICreateUserDTO): Promise<void> {
-    const numberRegexValidator = /[0-9]/;
-    const uppercaseRegexValidator = /[A-Z]/;
-
-    if (username.length < 3)
-      throw new ValidationError('Username must be 3 characters or more');
-
-    if (
-      password.length < 8 ||
-      !numberRegexValidator.test(password) ||
-      !uppercaseRegexValidator.test(password)
-    )
-      throw new ValidationError(
-        'Password must be 8 characters or more and contain a number and an uppercase letter'
-      );
-
-    const user = await prisma.user.findUnique({
-      where: {
-        username,
-      },
-    });
-
-    if (user != null) throw new ValidationError('Username already exists');
+    await validate({ username, password });
 
     const hashedPassword = await bcrypt.hash(password, 8);
 
