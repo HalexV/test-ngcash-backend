b22cc0bd2cc62577c9c8d7e13afbca7c80435113
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-18 09:10:10 -0400

    feat: it should throw a validation error when password does not contain a number

--- src/modules/user/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/user/useCases/createUser/CreateUserUseCase.ts
@@ -4,10 +4,12 @@ import ICreateUserDTO from '../../dtos/ICreateUserDTO';
 
 export default class CreateUserUseCase {
   async execute({ username, password }: ICreateUserDTO): Promise<void> {
+    const numberRegex = /[0-9]/;
+
     if (username.length < 3)
       throw new ValidationError('Username must be 3 characters or more');
 
-    if (password.length < 8)
+    if (password.length < 8 || !numberRegex.test(password))
       throw new ValidationError(
         'Password must be 8 characters or more and contain a number and an uppercase letter'
       );
