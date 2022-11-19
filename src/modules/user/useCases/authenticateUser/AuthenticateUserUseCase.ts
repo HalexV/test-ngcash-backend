f99c7f9a5782ea0772260bebc76208254eac5dad
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-19 10:08:07 -0400

    feat: it should throw a validation error when password is incorrect

--- src/modules/user/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/user/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -1,6 +1,7 @@
 import prisma from '../../../../client';
 import ValidationError from '../../../../errors/ValidationError';
 import IAuthenticateUserDTO from '../../dtos/IAuthenticateUserDTO';
+import bcrypt from 'bcrypt';
 
 export default class AuthenticateUserUseCase {
   async execute({ username, password }: IAuthenticateUserDTO): Promise<void> {
@@ -11,6 +12,11 @@ export default class AuthenticateUserUseCase {
     });
 
     if (user == null)
+      throw new ValidationError('Username or password is incorrect');
+
+    const compareResult = await bcrypt.compare(password, user.password);
+
+    if (!compareResult)
       throw new ValidationError('Username or password is incorrect');
   }
 }
