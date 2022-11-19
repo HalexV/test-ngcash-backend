4eef43eace71ff1060921c6969edffcfedac39ee
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-19 12:05:48 -0400

    refactor: move validations to validate function

--- src/modules/user/useCases/authenticateUser/AuthenticateUserUseCase.ts
+++ src/modules/user/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -9,21 +9,32 @@ const jwtSecret = process.env.JWT_SECRET ?? '12kflnfoi34493j0jkdsmalsaejp';
 interface Token {
   token: string;
 }
+
+async function validate({
+  username,
+  password,
+}: IAuthenticateUserDTO): Promise<any> {
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  });
+
+  if (user == null)
+    throw new ValidationError('Username or password is incorrect');
+
+  const compareResult = await bcrypt.compare(password, user.password);
+
+  if (!compareResult)
+    throw new ValidationError('Username or password is incorrect');
+
+  return {
+    user,
+  };
+}
 export default class AuthenticateUserUseCase {
   async execute({ username, password }: IAuthenticateUserDTO): Promise<Token> {
-    const user = await prisma.user.findUnique({
-      where: {
-        username,
-      },
-    });
-
-    if (user == null)
-      throw new ValidationError('Username or password is incorrect');
-
-    const compareResult = await bcrypt.compare(password, user.password);
-
-    if (!compareResult)
-      throw new ValidationError('Username or password is incorrect');
+    const { user } = await validate({ username, password });
 
     const token = await jwt.sign(
       {
