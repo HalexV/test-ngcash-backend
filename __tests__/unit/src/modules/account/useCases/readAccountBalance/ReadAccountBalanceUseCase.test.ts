846ce9c705fd8be83e6ed989bcbcb987983f3724
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-19 21:06:24 -0400

    test: it should throw a not found error if account does not exist

--- __tests__/unit/src/modules/account/useCases/readAccountBalance/ReadAccountBalanceUseCase.test.ts
+++ __tests__/unit/src/modules/account/useCases/readAccountBalance/ReadAccountBalanceUseCase.test.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { describe, it, expect, jest, beforeEach } from '@jest/globals';
 import prisma from '../../../../../../../src/client';
+import NotFoundError from '../../../../../../../src/errors/NotFoundError';
 import ReadAccountBalanceUseCase from '../../../../../../../src/modules/account/useCases/readAccountBalance/ReadAccountBalanceUseCase';
 
 describe('Account - Read Account Balance Use Case', () => {
@@ -43,5 +44,26 @@ describe('Account - Read Account Balance Use Case', () => {
     const promise = sut.execute(readAccountDTO);
 
     await expect(promise).rejects.toThrowError();
+  });
+
+  it('should throw a not found error if account does not exist', async () => {
+    const sut = new ReadAccountBalanceUseCase();
+
+    jest.spyOn(prisma.account, 'findUnique').mockResolvedValue(null);
+
+    const readAccountDTO = {
+      accountId: 'any',
+    };
+
+    let resultError;
+
+    try {
+      await sut.execute(readAccountDTO);
+    } catch (error: any) {
+      resultError = error;
+    }
+
+    expect(resultError).toBeInstanceOf(NotFoundError);
+    expect(resultError.message).toStrictEqual('Account not found');
   });
 });
