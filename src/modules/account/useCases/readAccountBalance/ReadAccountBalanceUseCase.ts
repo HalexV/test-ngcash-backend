debaa291e1a377654ad4095f8df2291c3981c2a5
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-19 21:13:40 -0400

    feat: it should return balance on success

--- src/modules/account/useCases/readAccountBalance/ReadAccountBalanceUseCase.ts
+++ src/modules/account/useCases/readAccountBalance/ReadAccountBalanceUseCase.ts
@@ -2,8 +2,11 @@ import prisma from '../../../../client';
 import NotFoundError from '../../../../errors/NotFoundError';
 import IReadAccountBalanceDTO from '../../dtos/IReadAccountBalanceDTO';
 
+interface Balance {
+  balance: number;
+}
 export default class ReadAccountBalanceUseCase {
-  async execute({ accountId }: IReadAccountBalanceDTO): Promise<void> {
+  async execute({ accountId }: IReadAccountBalanceDTO): Promise<Balance> {
     const account = await prisma.account.findUnique({
       where: {
         id: accountId,
@@ -11,5 +14,9 @@ export default class ReadAccountBalanceUseCase {
     });
 
     if (account == null) throw new NotFoundError('Account not found');
+
+    return {
+      balance: account.balance,
+    };
   }
 }
