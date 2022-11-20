319a87c4867945774380d5cad81bbdf74e545ef4
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-19 21:07:34 -0400

    feat: it should throw a not found error if account does not exist

--- src/modules/account/useCases/readAccountBalance/ReadAccountBalanceUseCase.ts
+++ src/modules/account/useCases/readAccountBalance/ReadAccountBalanceUseCase.ts
@@ -1,12 +1,15 @@
 import prisma from '../../../../client';
+import NotFoundError from '../../../../errors/NotFoundError';
 import IReadAccountBalanceDTO from '../../dtos/IReadAccountBalanceDTO';
 
 export default class ReadAccountBalanceUseCase {
   async execute({ accountId }: IReadAccountBalanceDTO): Promise<void> {
-    await prisma.account.findUnique({
+    const account = await prisma.account.findUnique({
       where: {
         id: accountId,
       },
     });
+
+    if (account == null) throw new NotFoundError('Account not found');
   }
 }
