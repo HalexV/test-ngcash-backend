123bcdde62178120c21e3936c2845fca30253b37
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-20 09:50:26 -0400

    refactor: move validations to validate function

--- src/modules/account/useCases/cashOutAccount/CashOutAccountUseCase.ts
+++ src/modules/account/useCases/cashOutAccount/CashOutAccountUseCase.ts
@@ -3,46 +3,59 @@ import NotFoundError from '../../../../errors/NotFoundError';
 import ValidationError from '../../../../errors/ValidationError';
 import ICashOutAccountDTO from '../../dtos/ICashOutAccountDTO';
 
+async function validate({
+  cashInUsername,
+  cashOutUser,
+  value,
+}: ICashOutAccountDTO): Promise<any> {
+  if (cashInUsername === cashOutUser.username)
+    throw new ValidationError('Cash out to yourself invalid');
+
+  const cashOutAccount = await prisma.account.findUnique({
+    where: {
+      id: cashOutUser.accountId,
+    },
+  });
+
+  if (cashOutAccount == null)
+    throw new NotFoundError('Cash out account not found');
+
+  if (value > cashOutAccount.balance)
+    throw new ValidationError('Balance insufficient');
+
+  const cashInAccountId = await prisma.user.findUnique({
+    where: {
+      username: cashInUsername,
+    },
+    select: {
+      accountId: true,
+    },
+  });
+
+  if (cashInAccountId == null)
+    throw new NotFoundError('Cash in username does not exist');
+
+  const cashInAccount = await prisma.account.findUnique({
+    where: {
+      id: cashInAccountId.accountId,
+    },
+  });
+
+  if (cashInAccount == null)
+    throw new NotFoundError('Cash in account not found');
+
+  return {
+    cashInAccount,
+    cashOutAccount,
+  };
+}
+
 export default class CashOutAccountUseCase {
   async execute({
     cashInUsername,
     cashOutUser,
     value,
   }: ICashOutAccountDTO): Promise<void> {
-    if (cashInUsername === cashOutUser.username)
-      throw new ValidationError('Cash out to yourself invalid');
-
-    const cashOutAccount = await prisma.account.findUnique({
-      where: {
-        id: cashOutUser.accountId,
-      },
-    });
-
-    if (cashOutAccount == null)
-      throw new NotFoundError('Cash out account not found');
-
-    if (value > cashOutAccount.balance)
-      throw new ValidationError('Balance insufficient');
-
-    const cashInAccountId = await prisma.user.findUnique({
-      where: {
-        username: cashInUsername,
-      },
-      select: {
-        accountId: true,
-      },
-    });
-
-    if (cashInAccountId == null)
-      throw new NotFoundError('Cash in username does not exist');
-
-    const cashInAccount = await prisma.account.findUnique({
-      where: {
-        id: cashInAccountId.accountId,
-      },
-    });
-
-    if (cashInAccount == null)
-      throw new NotFoundError('Cash in account not found');
+    await validate({ cashInUsername, cashOutUser, value });
   }
 }
