1680069445ea32322662b642d28cb9294dca8533
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-29 14:32:55 -0400

    test: it should throw a validation error if password is incorrect

--- __tests__/integration/modules/user/useCases/authenticateUser/AuthenticateUserUseCase.test.ts
+++ __tests__/integration/modules/user/useCases/authenticateUser/AuthenticateUserUseCase.test.ts
@@ -10,6 +10,7 @@ import {
 import prisma from '../../../../../../src/client';
 import AuthenticateUserUseCase from '../../../../../../src/modules/user/useCases/authenticateUser/AuthenticateUserUseCase';
 import ValidationError from '../../../../../../src/errors/ValidationError';
+import CreateUserUseCase from '../../../../../../src/modules/user/useCases/createUser/CreateUserUseCase';
 
 describe('Integration - User - Authenticate User Use Case', () => {
   beforeAll(async () => {
@@ -34,6 +35,36 @@ describe('Integration - User - Authenticate User Use Case', () => {
       username: 'testABC123',
       password: 'testZXC321',
     };
+
+    let resultError;
+
+    try {
+      await sut.execute(authenticateUserDTO);
+    } catch (error: any) {
+      resultError = error;
+    }
+
+    expect(resultError).toBeInstanceOf(ValidationError);
+    expect(resultError.message).toStrictEqual(
+      'Username or password is incorrect'
+    );
+  });
+
+  it('should throw a validation error if password is incorrect', async () => {
+    const createUser = new CreateUserUseCase();
+    const sut = new AuthenticateUserUseCase();
+
+    const createUserDTO = {
+      username: 'testABC123',
+      password: 'testZXC321',
+    };
+
+    const authenticateUserDTO = {
+      username: 'testABC123',
+      password: 'testZXC320',
+    };
+
+    await createUser.execute(createUserDTO);
 
     let resultError;
 
