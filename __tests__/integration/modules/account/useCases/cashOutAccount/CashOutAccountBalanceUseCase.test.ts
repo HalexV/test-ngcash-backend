4bd75ce298abade4c8227ae9691afab4f85aa744
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-29 16:47:26 -0400

    test: it should cash out a value to a username

--- __tests__/integration/modules/account/useCases/cashOutAccount/CashOutAccountBalanceUseCase.test.ts
+++ __tests__/integration/modules/account/useCases/cashOutAccount/CashOutAccountBalanceUseCase.test.ts
@@ -7,7 +7,7 @@ import {
   afterAll,
   afterEach,
 } from '@jest/globals';
-import { User } from '@prisma/client';
+import { Account, Transaction, User } from '@prisma/client';
 import prisma from '../../../../../../src/client';
 import NotFoundError from '../../../../../../src/errors/NotFoundError';
 import CashOutAccountUseCase from '../../../../../../src/modules/account/useCases/cashOutAccount/CashOutAccountUseCase';
@@ -21,8 +21,13 @@ describe('Integration - Account - Cash Out Account Use Case', () => {
   afterEach(async () => {
     const deleteUsers = prisma.user.deleteMany();
     const deleteAccounts = prisma.account.deleteMany();
+    const deleteTransactions = prisma.transaction.deleteMany();
 
-    await prisma.$transaction([deleteUsers, deleteAccounts]);
+    await prisma.$transaction([
+      deleteTransactions,
+      deleteUsers,
+      deleteAccounts,
+    ]);
   });
 
   afterAll(async () => {
@@ -90,5 +95,66 @@ describe('Integration - Account - Cash Out Account Use Case', () => {
     expect(resultError.message).toStrictEqual(
       'Cash in username does not exist'
     );
+  });
+
+  it('should cash out a value to a username', async () => {
+    const createUser = new CreateUserUseCase();
+    const sut = new CashOutAccountUseCase();
+
+    const createUserDTO = {
+      username: 'testABC123',
+      password: 'testCBA321',
+    };
+
+    const createUserDTO2 = {
+      username: 'testZXC123',
+      password: 'testCBA321',
+    };
+
+    await createUser.execute(createUserDTO);
+    await createUser.execute(createUserDTO2);
+
+    const cashOutUser = (await prisma.user.findUnique({
+      where: {
+        username: createUserDTO.username,
+      },
+    })) as User;
+
+    const cashInUser = (await prisma.user.findUnique({
+      where: {
+        username: createUserDTO2.username,
+      },
+    })) as User;
+
+    const cashOutAccountDTO = {
+      cashInUsername: cashInUser.username,
+      cashOutUser,
+      value: 40,
+    };
+
+    await sut.execute(cashOutAccountDTO);
+
+    const cashInAccount = (await prisma.account.findUnique({
+      where: {
+        id: cashInUser.accountId,
+      },
+    })) as Account;
+
+    const cashOutAccount = (await prisma.account.findUnique({
+      where: {
+        id: cashOutUser.accountId,
+      },
+    })) as Account;
+
+    const transaction = (await prisma.transaction.findFirst({
+      where: {
+        debitedAccountId: cashOutAccount.id,
+      },
+    })) as Transaction;
+
+    expect(cashInAccount.balance).toStrictEqual(140);
+    expect(cashOutAccount.balance).toStrictEqual(60);
+    expect(transaction.creditedAccountId).toStrictEqual(cashInAccount.id);
+    expect(transaction.debitedAccountId).toStrictEqual(cashOutAccount.id);
   });
 });
