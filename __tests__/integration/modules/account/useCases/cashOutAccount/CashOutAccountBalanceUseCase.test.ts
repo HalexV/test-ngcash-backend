e55213d6803556740c53b4ec44020ea3b50d230a
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-29 16:05:13 -0400

    test: it should throw a not found error if cash in username does not exist

--- __tests__/integration/modules/account/useCases/cashOutAccount/CashOutAccountBalanceUseCase.test.ts
+++ __tests__/integration/modules/account/useCases/cashOutAccount/CashOutAccountBalanceUseCase.test.ts
@@ -7,9 +7,11 @@ import {
   afterAll,
   afterEach,
 } from '@jest/globals';
+import { User } from '@prisma/client';
 import prisma from '../../../../../../src/client';
 import NotFoundError from '../../../../../../src/errors/NotFoundError';
 import CashOutAccountUseCase from '../../../../../../src/modules/account/useCases/cashOutAccount/CashOutAccountUseCase';
+import CreateUserUseCase from '../../../../../../src/modules/user/useCases/createUser/CreateUserUseCase';
 
 describe('Integration - Account - Cash Out Account Use Case', () => {
   beforeAll(async () => {
@@ -51,5 +53,42 @@ describe('Integration - Account - Cash Out Account Use Case', () => {
 
     expect(resultError).toBeInstanceOf(NotFoundError);
     expect(resultError.message).toStrictEqual('Cash out account not found');
+  });
+
+  it('should throw a not found error if cash in username does not exist', async () => {
+    const createUser = new CreateUserUseCase();
+    const sut = new CashOutAccountUseCase();
+
+    const createUserDTO = {
+      username: 'testABC123',
+      password: 'testCBA321',
+    };
+
+    await createUser.execute(createUserDTO);
+
+    const cashOutUser = (await prisma.user.findUnique({
+      where: {
+        username: createUserDTO.username,
+      },
+    })) as User;
+
+    const cashOutAccountDTO = {
+      cashInUsername: 'invalid',
+      cashOutUser,
+      value: 40,
+    };
+
+    let resultError;
+
+    try {
+      await sut.execute(cashOutAccountDTO);
+    } catch (error: any) {
+      resultError = error;
+    }
+
+    expect(resultError).toBeInstanceOf(NotFoundError);
+    expect(resultError.message).toStrictEqual(
+      'Cash in username does not exist'
+    );
   });
 });
