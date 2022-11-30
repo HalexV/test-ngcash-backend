ac14663d86fdf5d17a986fe353dedd938dc2a264
Halex Viotto Gomes <halexx002@gmail.com>
2022-11-30 16:43:00 -0400

    test: list transactions of user C on 2020-01-02 with cashout and cashin filters

--- __tests__/integration/modules/transaction/useCases/listTransactions/ListTransactionsUseCase.test.ts
+++ __tests__/integration/modules/transaction/useCases/listTransactions/ListTransactionsUseCase.test.ts
@@ -488,4 +488,32 @@ describe('Integration - Transaction - List Transactions Use Case', () => {
     expect(debited).toStrictEqual(7);
     expect(credited).toStrictEqual(2);
   });
+
+  it('should list all transactions of user C on 2020-01-02 when cash out and cash in filters are passed', async () => {
+    const sut = new ListTransactionsUseCase();
+
+    const transactions = await sut.execute({
+      accountId: userAccountC.id,
+      cashInTransactions: true,
+      cashOutTransactions: true,
+      transactionDate: new Date('2020-01-02'),
+    });
+
+    let debited = 0;
+    let credited = 0;
+
+    transactions.forEach((transaction) => {
+      if (transaction.creditedAccountId === userAccountC.id) {
+        credited++;
+      }
+
+      if (transaction.debitedAccountId === userAccountC.id) {
+        debited++;
+      }
+    });
+
+    expect(transactions.length).toStrictEqual(2);
+    expect(debited).toStrictEqual(0);
+    expect(credited).toStrictEqual(2);
+  });
 });
